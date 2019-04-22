eddbf954cfc5737fdc111c0ab21143530b8a4bb2
jblazing <john@johnpreynolds.com>
2019-04-22 15:18:17 -0400

    Added a git ignore to datafolder is pushed

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import cv2
 import models
 
-models.createModels((64,64,2))
+def main():
+
+    GenModel , DiscModel , AModel = models.createModels((64,64,2))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
